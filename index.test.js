236759575c5758e4024bd10cefd1bdf3189ca26f
JivanSalunke <jsalunke7197@gmail.com>
2024-02-17 12:33:48 +0530

    testing for multiple integers

--- index.test.js
+++ index.test.js
@@ -1,10 +1,9 @@
 function add(inputString) {
     let sum=0;
-    Array.from(inputString).forEach(char => {
-        if(!isNaN(char)){
-            sum+=Number(char);
-        }
-    });
+    let numArray=inputString.split(",");
+    for(let char of numArray){
+        sum+=Number(char);
+    }
     return sum;
 }
 
@@ -12,4 +11,6 @@ test("return sum of the integers when passes string of comma separated integers"
     expect(add("")).toBe(0);
     expect(add("1")).toBe(1);
     expect(add("1,2")).toBe(3);
+    expect(add("1,2,18,20,55")).toBe(96);
+
 });
