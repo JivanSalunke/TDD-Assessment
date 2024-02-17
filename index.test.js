4aa4ca6989ef92e87a0062461f62874c27d43dfb
JivanSalunke <jsalunke7197@gmail.com>
2024-02-17 12:11:21 +0530

    testing single integer string

--- index.test.js
+++ index.test.js
@@ -1,7 +1,10 @@
 function add(inputString) {
-    return 0;
+    let sum=Number(inputString);
+    console.log(sum);
+    return sum;
 }
 
 test("return sum of the integers when passes string of comma separated integers", () => {
     expect(add("")).toBe(0);
+    expect(add("1")).toBe(1);
 });
