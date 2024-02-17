c4adcb6aed38a60fe2b6b954b436ec0054948c9a
JivanSalunke <jsalunke7197@gmail.com>
2024-02-17 12:26:42 +0530

    testing two comma separated integers

--- index.test.js
+++ index.test.js
@@ -1,10 +1,15 @@
 function add(inputString) {
-    let sum=Number(inputString);
-    console.log(sum);
+    let sum=0;
+    Array.from(inputString).forEach(char => {
+        if(!isNaN(char)){
+            sum+=Number(char);
+        }
+    });
     return sum;
 }
 
 test("return sum of the integers when passes string of comma separated integers", () => {
     expect(add("")).toBe(0);
     expect(add("1")).toBe(1);
+    expect(add("1,2")).toBe(3);
 });
