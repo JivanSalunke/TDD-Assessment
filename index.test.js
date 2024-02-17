f7fc36c0e3cd20310600b69d457d4cbfa539ea5f
JivanSalunke <jsalunke7197@gmail.com>
2024-02-17 13:17:40 +0530

    testing for negative numbers

--- index.test.js
+++ index.test.js
@@ -1,6 +1,7 @@
 function add(inputString) {
     let sum=0;
-    let delimeter=","
+    let delimeter=",";
+    let negatives=[];
     if(inputString.match(/^[//]/)){
         delimeter=inputString[2];
         inputString=inputString.substr(2,);
@@ -8,12 +9,19 @@ function add(inputString) {
     let pattern=new RegExp(`[\n${delimeter}]+`);
     let numArray=inputString.split(pattern);
     for(let char of numArray){
-        sum+=Number(char);
+        if(Number(char)<0){
+            negatives.push(char);
+        }else{
+            sum+=Number(char);
+        }
+    }
+    if(negatives.length>0){
+        throw new Error("Negative numbers not allowed " +negatives.join(",") );
     }
     return sum;
 }
 
-test("return sum of the integers when passes string of comma separated or line separated integers", () => {
+test("return sum of the integers when passes string of any delimeter separated or line separated integers", () => {
     expect(add("")).toBe(0);
     expect(add("1")).toBe(1);
     expect(add("1,2")).toBe(3);
@@ -23,6 +31,5 @@ test("return sum of the integers when passes string of comma separated or line s
     expect(add("1\n2,3\n4,5\n6,8")).toBe(29);
     expect(add("//;\n1;2")).toBe(3);
     expect(add("//;\n1;2;3;4;5")).toBe(15);
-
-
+    expect(()=>add("10,-20,30,-1,-3,-5")).toThrow("Negative numbers not allowed -20,-1,-3,-5");
 }); 
